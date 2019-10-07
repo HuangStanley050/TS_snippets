217763386d0faab965e65b338ad34130a808842c
Stanley Huang <huang.stanley050@gmail.com>
2019-10-07 15:48:18 +1100

    array and tuple

---
+++ arrays.ts
@@ -0,0 +1,4 @@
+const carMakers = ["toyota", "honda", "ford"];
+const dates = [new Date(), new Date()];
+const carMakes = [["dddd"], ["ddaa"], ["adfd"]];
+const importantDates: (string | Date)[] = [];
---
+++ tuples.ts
@@ -0,0 +1,9 @@
+const drinks = {
+  color: "brown",
+  carbonated: true,
+  sugar: 40
+};
+type Drink = [string, boolean, number];
+const pepsi: Drink = ["brown", true, 40];
+const sprite: Drink = ["white", true, 50];
+const tea: Drink = ["black", false, 1];
